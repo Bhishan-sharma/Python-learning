d9d57d20d213cf601079ca862895cd635d2153d3
BhishanSharma <bhishansharma3354@gmail.com>
2023-03-03 16:52:38 +0530

    minor-change

--- noty.py
+++ noty.py
@@ -1,8 +1,11 @@
 import random
 import pyautogui as pg 
 import time
+import pygame
 
-time.sleep(8)
+clock = pygame.time.Clock()
+time.sleep(18)
 for i in range(500):
+    clock.tick(0.2)
     pg.write("Thankyou sir")
     pg.press("enter")
