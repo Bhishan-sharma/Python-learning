c5bb59fe305a8c4df6ade1b7ee4a378439e08ad8
BhishanSharma <bhishansharma3354@gmail.com>
2023-03-03 16:29:38 +0530

    update

--- noty.py
+++ noty.py
@@ -2,9 +2,7 @@ import random
 import pyautogui as pg 
 import time
 
-animal =("monkey","donkey","dog")
 time.sleep(8)
 for i in range(500):
-    a=random.choice(animal)
-    pg.write("you are a "+a)
+    pg.write("Thankyou sir")
     pg.press("enter")
